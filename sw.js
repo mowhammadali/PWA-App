ddbd3f303db6eac324be9c94a349f2fc7075857c
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2025-03-15 14:07:06 +0330

    cach all

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
 self.addEventListener("install", (event) => {
   console.log("The service worker installed successfully!");
   self.skipWaiting();
+
+  caches.open("pwa").then((cach) => {
+    cach.addAll(["./styles.css", "./js/app.js"]);
+  });
 });
 
 self.addEventListener("activate", (event) => {
@@ -9,4 +13,6 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   console.log(event);
+
+  event.respondWith(fetch(event.request));
 });
